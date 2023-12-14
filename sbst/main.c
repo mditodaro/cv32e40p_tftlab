d983fdb69121010a4b59d5922146ea4581f408b0
Riccardo Cantoro <riccardo.cantoro@polito.it>
2023-12-14 01:31:26 +0100

    created template for sbst

--- sbst/main.c
+++ sbst/main.c
@@ -1,32 +1,20 @@
-/*
- * Copyright 2020 ETH Zurich
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Author: Robert Balas <balasr@iis.ee.ethz.ch>
- */
+#include "tests.h"
 
-#include <stdio.h>
-#include <stdlib.h>
 
-//#include "mem_stall.h"
+const signature_t const GOLDEN_SIGNATURES[NUMTESTS] = {
+	0xCAFECAFE,	// TEST1
+};
 
-int main(int argc, char *argv[])
+int main()
 {
-#ifdef RANDOM_MEM_STALL
-    activate_random_stall();
-#endif
-    /* write something to stdout */
-    printf("hello world!\n");
-    return EXIT_SUCCESS;
+	int i, fails;
+	signature_t signatures[NUMTESTS];
+	
+	signatures[TEST1] = test1();
+
+	for(i=0; i<NUMTESTS; i++) {
+		fails += signatures[i] != GOLDEN_SIGNATURES[i];
+	}
+
+ 	return fails;
 }
